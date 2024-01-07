b149af3c14cad82e54673c00685eab8457e61d59
Zinjurke18 <suvarnazinjurke58879@gmail.com>
2024-01-07 10:36:57 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,20 +1,100 @@
 const express = require("express"); //express package initiated
 const app = express(); // express instance has been created and will be access by app variable
 const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
 dotenv.config();
+
+app.set("view engine","ejs");
 
 const connection = require("./config/db")
 
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/create.html");
 });
 
-app.post("/create", (req, res) => {
+app.get("/delete-data", (req, res) => {
+  const deleteData = "delete from youtube_table where id=?";
+  connection.query(deleteData, [req.query.id], (err, rows) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect("/data");
+    }
+  });
+});
+
+
+app.get("/data",(req,res)=>{
+connection.query("select * from youtube_table",(err,rows)=>{
+  if(err){
+    console.log(err)
+  }
+  else{
+    res.render("read.ejs",{rows});
+  }
+});
+});
+
+app.get("/update-data",(req,res)=>{
+  connection.query("select * from youtube_table where id= ?",
+   [req.query.id],
+   (err,eachRow)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      result = JSON.parse(JSON.stringify(eachRow[0]));
+      console.log(result);
+      res.render("edit.ejs",{result});
+    }
+  })
+})
+
+app.post("/final-update", (req, res) => {
+  console.log(req.body);
+  const id = req.body.hidden_id;
+  const name = req.body.name;
+  const email = req.body.email;
+
+console.log("id.....",id);
+
+  const updateQuery = "update youtube_table set name=?, email=? where id=?";
     try{
-    console.log("create");
-    res.redirect("/");
+   connection.query(updateQuery,
+   [name,email,id],(err,rows)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.redirect("/data")
+    }
+   });
+    }
+    catch(err){
+    console.log(err);
+    }
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+
+  const name = req.body.name;
+  const email = req.body.email;
+    try{
+   connection.query("INSERT into youtube_table(name,email) values(?,?)",
+   [name,email],(err,rows)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.redirect("/data")
+    }
+   });
     }
     catch(err){
     console.log(err);
