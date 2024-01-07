c199c8d78df88ec3c8edcf607ae7f7c84a38c495
adhamale2810 <adhamale78@gmail.com>
2024-01-07 16:03:17 +0530

    Setting up for next progress

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 const connection = mysql.createConnection({
- host: process.env.HOST,
- user: process.env.USER,
- password: process.env.PASSWORD,
- database: process.env.DATABASE,
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
 });
 connection.connect((error) => {
-if (error) throw error;
-console.log("connection succesfull");
+	if (error) throw error;
+	console.log("connection succesfull");
 })
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const dotenv = require("dotenv");
 var bodyParser = require("body-parser");
 dotenv.config();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-const connection = require("./config/db")
+const connection = require("./config/db");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
@@ -29,30 +29,30 @@ app.get("/delete-data", (req, res) => {
 });
 
 
-app.get("/data",(req,res)=>{
-connection.query("select * from youtube_table",(err,rows)=>{
-  if(err){
-    console.log(err)
-  }
-  else{
-    res.render("read.ejs",{rows});
-  }
-});
+app.get("/data", (req, res) => {
+  connection.query("select * from youtube_table", (err, rows) => {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      res.render("read.ejs", { rows });
+    }
+  });
 });
 
-app.get("/update-data",(req,res)=>{
+app.get("/update-data", (req, res) => {
   connection.query("select * from youtube_table where id= ?",
-   [req.query.id],
-   (err,eachRow)=>{
-    if(err){
-      console.log(err);
-    }
-    else{
-      result = JSON.parse(JSON.stringify(eachRow[0]));
-      console.log(result);
-      res.render("edit.ejs",{result});
-    }
-  })
+    [req.query.id],
+    (err, eachRow) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        result = JSON.parse(JSON.stringify(eachRow[0]));
+        console.log(result);
+        res.render("edit.ejs", { result });
+      }
+    })
 })
 
 app.post("/final-update", (req, res) => {
@@ -61,23 +61,23 @@ app.post("/final-update", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
 
-console.log("id.....",id);
+  console.log("id.....", id);
 
   const updateQuery = "update youtube_table set name=?, email=? where id=?";
-    try{
-   connection.query(updateQuery,
-   [name,email,id],(err,rows)=>{
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.redirect("/data")
-    }
-   });
-    }
-    catch(err){
+  try {
+    connection.query(updateQuery,
+      [name, email, id], (err, rows) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          res.redirect("/data")
+        }
+      });
+  }
+  catch (err) {
     console.log(err);
-    }
+  }
 });
 
 app.post("/create", (req, res) => {
@@ -85,23 +85,23 @@ app.post("/create", (req, res) => {
 
   const name = req.body.name;
   const email = req.body.email;
-    try{
-   connection.query("INSERT into youtube_table(name,email) values(?,?)",
-   [name,email],(err,rows)=>{
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.redirect("/data")
-    }
-   });
-    }
-    catch(err){
+  try {
+    connection.query("INSERT into youtube_table(name,email) values(?,?)",
+      [name, email], (err, rows) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          res.redirect("/data")
+        }
+      });
+  }
+  catch (err) {
     console.log(err);
-    }
+  }
 });
 app.listen(process.env.PORT || 4000, (error) => {
-    if( error) throw error;
+  if (error) throw error;
 
-    console.log(`server running on ${process.env.PORT}`);
+  console.log(`server running on ${process.env.PORT}`);
 });
